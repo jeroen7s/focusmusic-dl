f6a1cf48f04309765365da645e0e67056ddd30e3
Jeroen Evens <jeroen7e@gmail.com>
2020-02-15 16:16:14 +0100

    remove os import, check if file already exists

--- focusmusic-get-songs.py
+++ focusmusic-get-songs.py
@@ -1,26 +1,23 @@
-import json
-import os
-import requests
+import json, requests
 from pathlib import Path
 from urllib.request import urlretrieve
 from multiprocessing import Pool
 
 def download_channel(channel, offset=0, firsturl=False):
-    Path(os.getcwd() + "/" + channel).mkdir(parents=True, exist_ok=True)
+    Path(str(Path()) + "/" + channel).mkdir(parents=True, exist_ok=True)
+
     while 1:
         url = f'https://focusmusic.fm/api/tracks.php?offset={offset}&channel={channel}'
         response_json = json.loads(requests.get(url, timeout=10).content)
         file_url = response_json.get("url").replace("\\","")
-        if file_url == firsturl:
-            break
+        filepath = str(Path()) + "/" + channel + "/" + file_url.split("/")[-1]
+
+        if file_url == firsturl: break
         firsturl = firsturl or file_url
-        filename = file_url.split("/")[-1]
-        filepath = os.getcwd() + "/" + channel + "/" + filename
-        print("getting offset {} filename {}".format(offset,filename))
-        try:
-            urlretrieve (file_url, filepath)
-        except Exception:
-            print("File not Found")
+
+        if not Path(filepath).exists():
+            print("getting offset {} filename {}".format(offset, Path(filepath).name))
+            urlretrieve(file_url, filepath)
         offset +=1
 
 Pool().map(download_channel, ["electronic", "downtempo", "classical", "rain"])
